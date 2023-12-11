4b8be5d746e8461a295ea3482f0c4d2519d1ba11
Filip Průcha <pruchfi1@fit.cvut.cz>
2023-12-11 15:48:07 +0100

    delta time, only alphanumeric

--- sfml_matrix/sfml_matrix.cpp
+++ sfml_matrix/sfml_matrix.cpp
@@ -3,96 +3,113 @@
 #include <Windows.h>
 #include <random>
 
-std::vector<std::vector<sf::Text>> texts;
-
 int main() {
-    ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
+    std::vector<std::vector<sf::Text>> texts;
 
-    sf::RenderWindow window(sf::VideoMode(), "SFML works!", sf::Style::Fullscreen);
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+
+    constexpr float speed = 10.0;
+
+    const uint32_t windowSizeX = sf::VideoMode::getDesktopMode().width;
+    const uint32_t windowSizeY = sf::VideoMode::getDesktopMode().height;
+
+    sf::RenderWindow window(sf::VideoMode(windowSizeX, windowSizeY), "Matrix", sf::Style::Fullscreen);
 
     sf::Font font;
     font.loadFromFile("./Antimatrix-BWBVw.ttf");
 
     std::mt19937 mt((std::random_device())());
-    std::uniform_int_distribution< std::mt19937::result_type> textHeight(30, 100);
-    std::uniform_int_distribution< std::mt19937::result_type> textChars('0', 'z');
-    std::uniform_int_distribution< std::mt19937::result_type> textSize(15, 25);
-    std::uniform_int_distribution< std::mt19937::result_type> width(0, window.getSize().x);
+    std::uniform_int_distribution<std::mt19937::result_type> textHeightUid(windowSizeY / 50u, windowSizeY / 10u);
+    std::uniform_int_distribution<std::mt19937::result_type> textCharsUid('0', 'z');
+    std::uniform_int_distribution<std::mt19937::result_type> textSizeUid(windowSizeX / 150u, windowSizeX / 60u);
+    std::uniform_int_distribution<std::mt19937::result_type> textXUid(0u, windowSizeX);
 
-    for (size_t i = 0; i < window.getSize().x / 15; i++) {
+    texts.reserve(windowSizeX / 15);
+
+    for (size_t i = 0ull; i < windowSizeX / 15; ++i) {
         std::vector<sf::Text> innerVec;
 
-        uint32_t height = textHeight(mt);
-        uint32_t size = textSize(mt);
-        uint32_t x = width(mt);
+        const uint8_t height = textHeightUid(mt);
+        const uint32_t size = textSizeUid(mt);
+        const uint32_t x = textXUid(mt);
 
-        for (size_t j = 0; j < height; j++) {
+        innerVec.reserve(height);
+
+        for (uint8_t j = 0; j < height; ++j) {
             sf::Color color((255 / height) * j, 255, (255 / height) * j);
 
-            char c = textChars(mt);
-            std::string str; str += c;
-            sf::Text text(str, font);
+            char textChar;
+            do {
+                textChar = textCharsUid(mt);
+            } while (!isalnum(textChar));
+
+            sf::Text text(textChar, font);
             text.setFillColor(color);
             text.setCharacterSize(size);
-            text.setPosition(x, j * size);
-            innerVec.push_back(text);
+
+            text.setPosition(static_cast<float>(x), static_cast<float>(j) * size);
+
+            innerVec.push_back(std::move(text));
         }
 
-        texts.push_back(innerVec);
+        texts.push_back(std::move(innerVec));
     }
 
+    sf::Clock deltaClock;
+
     while (window.isOpen()) {
+        sf::Time deltaTime = deltaClock.restart();
         sf::Event event;
 
-        for (size_t i = 0ull; i < texts.size(); ++i) {
-            bool outOfBounds = texts[i][0].getPosition().y > window.getSize().y;
-
-            if (outOfBounds) {
-                texts[i].clear();
-
-                std::vector<sf::Text> innerVec;
-
-                uint32_t height = textHeight(mt);
-                uint32_t size = textSize(mt);
-                uint32_t x = width(mt);
-
-                for (size_t j = 0; j < height; j++) {
-                    sf::Color color((255 / height) * j, 255, (255 / height) * j);
-
-                    char c = textChars(mt);
-                    std::string str; str += c;
-                    sf::Text text(str, font);
-                    text.setFillColor(color);
-                    text.setCharacterSize(size);
-                    text.setPosition(x, static_cast<int32_t>(j) * static_cast<int32_t>(size) - static_cast<int32_t>(height) * static_cast<int32_t>(size));
-                    innerVec.push_back(text);
-                }
-
-                texts[i] = std::move(innerVec);
-            }
-        }
+        window.clear();
 
         for (auto& vec : texts) {
             for (auto& text : vec) {
                 sf::Vector2f pos = text.getPosition();
-                text.setPosition(pos.x, pos.y + text.getCharacterSize());
-            }
-        }
-
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-
-        for (const auto& vec : texts) {
-            for (const auto& text : vec) {
+                text.setPosition(pos.x, pos.y + (text.getCharacterSize() * deltaTime.asSeconds() * speed));
                 window.draw(text);
+            }
+
+            bool outOfBounds = vec[0].getPosition().y > windowSizeY;
+
+            if (outOfBounds) {
+                vec.clear();
+
+                std::vector<sf::Text> innerVec;
+
+                const uint8_t height = textHeightUid(mt);
+                const uint32_t size = textSizeUid(mt);
+                const uint32_t x = textXUid(mt);
+
+                vec.reserve(height);
+
+                for (uint8_t j = 0; j < height; ++j) {
+                    sf::Color color((255 / height) * j, 255, (255 / height) * j);
+
+                    char textChar;
+                    do {
+                        textChar = textCharsUid(mt);
+                    } while (!isalnum(textChar));
+
+                    sf::Text text(textChar, font);
+                    text.setFillColor(color);
+                    text.setCharacterSize(size);
+
+                    float newY = j * static_cast<float>(size) - static_cast<float>(height) * size;
+                    text.setPosition(static_cast<float>(x), newY);
+
+                    vec.push_back(std::move(text));
+                }
             }
         }
 
         window.display();
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
     }
 
     return 0;
